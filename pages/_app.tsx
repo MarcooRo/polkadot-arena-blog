7e191bc19c1972f14d2dbfd2170d0ec729722cdf
MarcoRo <marcoro@MacBook-Pro-di-MarcoRo.local>
2022-10-21 17:41:48 +0200

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import {theme} from '../styles/theme'
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- styles/theme.ts
+++ styles/theme.ts
@@ -18,6 +18,7 @@ export const theme = extendTheme({
             }
         }
       }
+      
     },
   },
 })
