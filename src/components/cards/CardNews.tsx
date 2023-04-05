c200b8f4978980f33298ef269909c79861df8b24
MarcoRo <marcoro@MacBook-Pro-di-MarcoRo.local>
2023-04-05 22:16:41 +0200

    update

--- src/components/cards/CardNews.tsx
+++ src/components/cards/CardNews.tsx
@@ -142,7 +142,6 @@ const CardComponent: React.FC<ITcard> = (props) => {
                      {props.ownedByAccount.profileSpace?.name}
                   </Text>
                )}
-               {/* <Text color={'gray.500'}>{date.toLocaleDateString()}</Text> */}
             </Stack>
          </Stack>
       </Box>
